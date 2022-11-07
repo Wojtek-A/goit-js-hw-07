d6c8a065262099ad7bc2c3ea76e485f0a095d780
Wojtek-A <wojtek.a@hotmail.com>
2022-11-07 21:45:40 +0100

    Poprawki

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,9 +1,7 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
-// console.log(galleryItems);
 
 const instance = basicLightbox.create(`<img src="" alt=""/>`);
-instance.show();
 const gallery = document.querySelector(".gallery");
 
 const images = galleryItems
@@ -17,16 +15,14 @@ gallery.insertAdjacentHTML("afterbegin", images);
 
 gallery.addEventListener("click", (event) => {
   event.preventDefault();
+
   const clickedElement = event.target;
   if (clickedElement.nodeName !== "IMG") return;
-  console.log(clickedElement);
 
   const largeImage = clickedElement.dataset.source;
   const alt = clickedElement.alt;
-
   const modal = instance.element();
   const modalImgage = modal.querySelector("IMG");
-  console.log(modalImgage);
   modalImgage.src = largeImage;
   modalImgage.alt = alt;
 
